58594ecd829d450b47cfa418ff5bd87cda0aa6d7
Anthony Wong <anthonywong555@gmail.com>
2021-04-28 15:13:20 -0400

    Add functionality that will auto fill in the display name

--- src/state/useSalesforceAuth/useSalesforceAuth.ts
+++ src/state/useSalesforceAuth/useSalesforceAuth.ts
@@ -1,34 +1,79 @@
 import { useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
+import { RoomType } from '../../types';
 import LCC from 'lightning-container';
+
+const getRunningUser = () => {
+  return new Promise<string>((resolve, reject) => {
+    LCC.callApex(
+      'TwilioVideoController.getRunningUser',
+      [],
+      (result, event) => {
+        if (result) {
+          resolve(result);
+        } else if (event.type === 'exception') {
+          reject(event);
+        }
+      },
+      { escape: false }
+    );
+  });
+};
+
+export function fetchToken(
+  identity: string,
+  roomName: string,
+  createRoom = true,
+  createConversation = process.env.REACT_APP_DISABLE_TWILIO_CONVERSATIONS !== 'true'
+) {
+  return new Promise<string>((resolve, reject) => {
+    LCC.callApex(
+      'TwilioVideoController.getAccessTokenForRemote',
+      JSON.stringify({
+        identity,
+        roomName,
+        createRoom,
+        createConversation,
+      }),
+      (result, event) => {
+        if (result) {
+          resolve(result);
+        } else if (event.type === 'exception') {
+          reject(event);
+        }
+      },
+      { escape: false }
+    );
+  });
+}
 
 export default function useSalesforceAuth() {
   const history = useHistory();
 
   const [user, setUser] = useState<{ displayName: undefined; photoURL: undefined; passcode: string } | null>(null);
-  const [isAuthReady, setIsAuthReady] = useState(true);
+  const [isAuthReady, setIsAuthReady] = useState(false);
+  const [roomType, setRoomType] = useState<RoomType>();
 
   const getToken = useCallback((name: string, room: string) => {
-    return new Promise<string>((resolve, reject) => {
-      LCC.callApex(
-        'TwilioVideoController.getAccessTokenForRemote',
-        JSON.stringify({ identity: name, roomName: room }),
-        (result, event) => {
-          if (result) {
-            resolve(result);
-          } else if (event.type === 'exception') {
-            reject(event);
-          }
-        },
-        []
-      );
+    return fetchToken(name, room).then(result => {
+      const something = JSON.parse(result);
+      const { roomType, token } = something;
+      setRoomType(roomType);
+      return token as string;
     });
   }, []);
 
   useEffect(() => {
-    setUser({ passcode: null } as any);
-    setIsAuthReady(true);
+    getRunningUser()
+      .then(salesforceUserJSON => {
+        const salesforceUser = JSON.parse(salesforceUserJSON);
+        setUser({ displayName: salesforceUser.name } as any);
+        setIsAuthReady(true);
+      })
+      .catch(e => {
+        console.error(e);
+      });
   }, [history]);
 
-  return { isAuthReady, getToken, user };
+  return { getToken, user, isAuthReady, roomType };
 }
